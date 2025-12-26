b15ae16dbe2a8994a124fdb73bab7c9890cc3fd1
Pasindu_SD <dilharawijesinghe86@gmail.com>
2025-12-26 12:46:20 +0530

    Summery Update
    Error fixing and, add some new part

---
+++ client_email_config.py
@@ -0,0 +1,11 @@
+ # Configure your email here
+EMAIL = "dilharawijesinghe86@gmail.com"
+PASSWORD = "sobo jezh mjmx hvmr"
+IMAP_SERVER = "imap.gmail.com"
+
+def get_config():
+    return {
+        "email": EMAIL,
+        "password": PASSWORD,
+        "imap_server": IMAP_SERVER
+    }
